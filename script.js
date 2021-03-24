39efd4d47d26d6e60a51729cc0bbec77632f8710
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-24 14:43:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let input = 0
+
+document.getElementById('button')
+.addEventListener('click', enter)
+
+function enter () {
+   input = document.getElementById('input').value 
+   if (input >= 17) { 
+     alert('you can watch any movie') 
+   } else if (input >= 13) {
+     alert('ypu can watch a pg 13 movie')
+   } else if (input >= 5) {
+     alert('you can watch ao or G movie')
+   } else {
+     alert('you can not watch this movie you are too young')
+ }
+}
